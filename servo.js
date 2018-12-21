92f3b254ecfa64883577d238f410f6d6a6d60bd0
RBlanchet <contact@blanchet-romain.fr>
2018-12-21 15:21:14 +0100

    servo.js contrl z

--- servo.js
+++ servo.js
@@ -1,5 +1,9 @@
+const Gpio = require('pigpio').Gpio;
 
+const motor = new Gpio(6, {mode: Gpio.OUTPUT});
 
+let pulseWidth = 1000;
+let increment = 100;
 
 setInterval(() => {
   motor.servoWrite(pulseWidth);
